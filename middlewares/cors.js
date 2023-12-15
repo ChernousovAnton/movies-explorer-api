780491e11ccd461bce49ef214aa0c028e989eac4
ChernousovAnton <Chantony_uz@mail.ru>
2023-12-15 21:21:22 +0300

    cors allowed all

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -5,20 +5,21 @@ const allowedCors = [
 
 // eslint-disable-next-line consistent-return
 const corsHandler = (req, res, next) => {
-  const { origin } = req.headers;
-  const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
+  // const { origin } = req.headers;
+  // const { method } = req;
+  // const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  // const requestHeaders = req.headers['access-control-request-headers'];
 
-  if (allowedCors.includes(origin)) {
-    if (method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-      res.header('Access-Control-Allow-Headers', requestHeaders);
-      res.header('Access-Control-Allow-Origin', origin);
-      return res.end();
-    }
-    res.header('Access-Control-Allow-Origin', origin);
-  }
+  // if (allowedCors.includes(origin)) {
+  //   if (method === 'OPTIONS') {
+  //     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+  //     res.header('Access-Control-Allow-Headers', requestHeaders);
+  //     res.header('Access-Control-Allow-Origin', origin);
+  //     return res.end();
+  //   }
+  //   res.header('Access-Control-Allow-Origin', origin);
+  // }
+  res.header('Access-Control-Allow-Origin', '*');
   next();
 };
 
