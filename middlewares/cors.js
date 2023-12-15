00c93f0e360fece69497c5eaafa10311d58abef9
ChernousovAnton <Chantony_uz@mail.ru>
2023-12-15 21:47:02 +0300

    cors change

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -5,10 +5,10 @@ const allowedCors = [
 
 // eslint-disable-next-line consistent-return
 const corsHandler = (req, res, next) => {
-  // const { origin } = req.headers;
-  // const { method } = req;
-  // const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  // const requestHeaders = req.headers['access-control-request-headers'];
+  const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
 
   // if (allowedCors.includes(origin)) {
   //   if (method === 'OPTIONS') {
@@ -19,6 +19,12 @@ const corsHandler = (req, res, next) => {
   //   }
   //   res.header('Access-Control-Allow-Origin', origin);
   // }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.header('Access-Control-Allow-Origin', '*');
+    return res.end();
+  }
   res.header('Access-Control-Allow-Origin', '*');
   next();
 };
